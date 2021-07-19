f1a65c393506a1da3b3166fcac1e80d04febd346
Nikola Penkov <nikolapenkovfx@gmail.com>
2021-07-19 18:43:47 +0100

    tets

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <article>
+        <h1><a href="/post"> My First Post </a></h1>
+
+        <?= $post; ?>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <article>
+        <h1><a href="/post"> My First Post </a></h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Esse recusandae cupiditate libero, itaque voluptate facilis 
+            vel officia earum excepturi iusto magnam iure ad assumenda quia 
+            quibusdam. Doloremque, tempore. Beatae, quidem!</p>
+    </article>
+
+
+</body>
